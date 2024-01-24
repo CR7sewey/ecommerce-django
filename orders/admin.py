73c2637894fcd9d0d9c4c29ae110d879340c7c08
CR7sewey <miguelmotacarvalho@gmail.com>
2024-01-24 14:05:20 +0000

    Models Order and ItemOrder

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from orders.models import ItemOrder, Order
 
 # Register your models here.
+
+
+@admin.register(ItemOrder)
+class ItemOrderAdmin(admin.ModelAdmin):
+    ...
+
+# TO put variation inline in admin (in product)
+
+
+class ItemOrderInline(admin.TabularInline):
+    model = ItemOrder
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = ItemOrderInline,
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Order(models.Model):
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        blank=True, null=True,
+        # para relacao invertida/inversa - user.post_set.all() -> user.post_create_by.QUERYSET!
+        related_name='user_order'
+    )
+
+    total = models.FloatField(verbose_name="Total")
+    status = models.CharField(
+        default='C',
+        max_length=1,
+        choices=(
+            ('A', 'Aprovado'),
+            ('C', 'Criado'),
+            ('R', 'Reprovado'),
+            ('P', 'Pendente'),
+            ('E', 'Enviado'),
+            ('F', 'Finalizado'),
+        )
+    )
+
+    def __str__(self) -> str:
+        return f'Order: {self.pk}'
+
+
+class ItemOrder(models.Model):
+    class Meta:
+        verbose_name = 'ItemOrder'
+        verbose_name_plural = 'ItemOrders'
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.CharField(max_length=255)  # name of product
+    product_id = models.PositiveIntegerField()
+    variation = models.CharField(max_length=255)  # name of variation
+    variation_id = models.PositiveIntegerField()
+    price = models.FloatField()
+    promotion_price = models.FloatField(default=0)  # talvez remova
+    quantity = models.PositiveIntegerField()
+    image = models.CharField(max_length=2000)  # caminho da imagem!!
+
+    def __str__(self) -> str:
+        return f'Item of {self.order}'
